40e4a2758059300f385c4ad80771ec5ecff3e614
Woo Joo Chae <enaenen@naver.com>
2022-09-06 01:57:50 +0900

    ex01

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include "Form.hpp"
+
+#define LOWEST 150
+#define HIGHEST 1
+
+class Form;
+
+class Bureaucrat {
+   private:
+	const std::string name;
+	int grade;
+
+   public:
+	Bureaucrat( void );
+	Bureaucrat( std::string name, int grade );
+	Bureaucrat( const Bureaucrat &ref );
+	Bureaucrat &operator=( const Bureaucrat &ref );
+	~Bureaucrat( void );
+
+	class GradeTooHighException : public std::exception {
+	   public:
+		const char *what( void ) const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	   public:
+		const char *what( void ) const throw();
+	};
+
+	std::string getName() const;
+	int getGrade() const;
+
+	void promoteGrade( void );
+	void demoteGrade( void );
+};
+std::ostream &operator<<( std::ostream &os, const Bureaucrat &b );
---
+++ ex01/Form.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+   private:
+	const std::string name;
+	bool signFlag;
+	const int signGrade;
+	const int executeGrade;
+
+   public:
+	class GradeTooHighException : public std::exception {
+	   public:
+		const char *what( void ) const throw();
+	};
+	class GradeTooLowException : public std::exception {
+	   public:
+		const char *what( void ) const throw();
+	};
+
+	Form( void );
+	Form( std::string name, int signGrade, int executeGrade );
+	Form( Form const &ref );
+	Form &operator=( Form const &ref );
+	~Form( void );
+
+	const std::string &getName( void ) const;
+	bool isSigned( void ) const;
+	int getSigneGrade( void ) const;
+	int getExecuteGrade( void ) const;
+};
+std::ostream &operator<<( std::ostream &os, Form const &f );
